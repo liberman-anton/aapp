ed1266e19406fb85affe47959ca4cd98e8d94c2e
Liberman Anton <antonliberman@gmail.com>
2020-04-08 14:43:31 +0300

    Update TecmintApp.java

--- TecmintApp.java
+++ TecmintApp.java
@@ -1,5 +1,19 @@
+import java.io.*;
+import java.util.*;
+
 public class TecmintApp {
-		public static void main(String[] args){
-					System.out.println(" Just executed TecmintApp! ");
-						}
+ public static void main(String[] args){
+  System.out.println("start");
+  BufferedReader input = new BufferedReader(new FileReader(args[0]));
+  
+  List<String> lines = new LinkedList<>();
+  while(true){
+	String line = input.readLine();
+	if(line == null) break;
+	lines.add(line);
+  }
+  input.close();
+  System.out.println("good bye");
+  System.out.println("finish");
+ }
 }
