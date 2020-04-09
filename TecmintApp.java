2da7edbb4023257e23ffdad1bd53c82c4b46215a
Liberman Anton <antonliberman@gmail.com>
2020-04-09 14:31:00 +0300

    Update TecmintApp.java

--- TecmintApp.java
+++ TecmintApp.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.util.*;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import javax.net.ssl.HttpsURLConnection;
+import java.net.*;
 
 public class TecmintApp {
  public static void main(String[] args) throws Exception {
@@ -18,52 +15,10 @@ public class TecmintApp {
   }
   input.close();
   System.out.println("list: " + lines);
-  new TecmintApp().sendPost();
+  
+
   System.out.println("finish");
  }
 
-private void sendPost() throws Exception {
-
-	// url is missing?
-        //String url = "https://selfsolve.apple.com/wcResults.do";
-        String url = "https://httpbin.org/post";
-
-        HttpsURLConnection httpClient = (HttpsURLConnection) new URL(url).openConnection();
-
-        //add reuqest header
-        httpClient.setRequestMethod("POST");
-        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
-        httpClient.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
-
-        // Send post request
-        httpClient.setDoOutput(true);
-        try (DataOutputStream wr = new DataOutputStream(httpClient.getOutputStream())) {
-            wr.writeBytes(urlParameters);
-            wr.flush();
-        }
-
-        int responseCode = httpClient.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post parameters : " + urlParameters);
-        System.out.println("Response Code : " + responseCode);
-
-        try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(httpClient.getInputStream()))) {
-
-            String line;
-            StringBuilder response = new StringBuilder();
-
-            while ((line = in.readLine()) != null) {
-                response.append(line);
-            }
-
-            //print result
-            System.out.println(response.toString());
-
-        }
-
-    }
 
 }
