c9eb6e433c756deff15dc374cc845a587d423203
Liberman Anton <antonliberman@gmail.com>
2020-04-09 14:34:15 +0300

    Update TecmintApp.java

--- TecmintApp.java
+++ TecmintApp.java
@@ -16,9 +16,49 @@ public class TecmintApp {
   input.close();
   System.out.println("list: " + lines);
   
-
+  new TecmintApp().sendPost();
+  
   System.out.println("finish");
  }
 
+private void sendPost() throws Exception {
+
+        // form parameters
+        Map<Object, Object> data = new HashMap<>();
+        data.put("username", "abc");
+        data.put("password", "123");
+        data.put("custom", "secret");
+        data.put("ts", System.currentTimeMillis());
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(buildFormDataFromMap(data))
+                .uri(URI.create("https://httpbin.org/post"))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print status code
+        System.out.println(response.statusCode());
+
+        // print response body
+        System.out.println(response.body());
+
+    }
+
+    private static HttpRequest.BodyPublisher buildFormDataFromMap(Map<Object, Object> data) {
+        var builder = new StringBuilder();
+        for (Map.Entry<Object, Object> entry : data.entrySet()) {
+            if (builder.length() > 0) {
+                builder.append("&");
+            }
+            builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
+            builder.append("=");
+            builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
+        }
+        System.out.println(builder.toString());
+        return HttpRequest.BodyPublishers.ofString(builder.toString());
+    }
 
 }
