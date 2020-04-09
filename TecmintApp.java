cbb71dbfbe371dde8ecf0a0c7c21e2a9dca5229e
Liberman Anton <antonliberman@gmail.com>
2020-04-09 16:35:52 +0300

    Update TecmintApp.java

--- TecmintApp.java
+++ TecmintApp.java
@@ -7,6 +7,9 @@ import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+import static java.net.http.HttpResponse.*;
+
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
